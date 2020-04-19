60f27ea08fffbb8efc8c27770cb375b6c8c7bba5
sgwicks <samgwicks@gmail.com>
2020-04-19 20:46:43 +0100

    Copied about.html to About.jsx. Imported Reach Router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import { Router } from '@reach/router';
+import About from './components/About';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Home />
+      <Router className='router'>
+        <Home path='/' />
+        <About path='/about' />
+      </Router>
       <Footer />
     </div>
   );
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 const Header = () => {
   return (
@@ -6,8 +7,8 @@ const Header = () => {
       <h1 className='header-text'>SG Wicks</h1>
       <h2 className='header-text'>Developer, Writer, Student</h2>
       <nav>
-        <a href='index.html'>Home</a> | <a href='portfolio.html'>Portfolio</a> |{' '}
-        <a href='about.html'>About</a> | <a href='contact.html'>Contact</a>
+        <Link to='/'>Home</Link> |<Link to='/portfolio'>Portfolio</Link> |{' '}
+        <Link to='/about'>About</Link> | <Link to='/contact'>Contact</Link>
       </nav>
     </header>
   );
