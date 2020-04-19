beea566446d23fc77a720c55fb972b2d80aa8faa
sgwicks <samgwicks@gmail.com>
2020-04-19 21:03:58 +0100

    Moved div boxes into a tags to solve warnings. To be converted later to components and Links

--- portfolio/src/components/Portfolio.jsx
+++ portfolio/src/components/Portfolio.jsx
@@ -7,47 +7,49 @@ const Portfolio = () => {
         <h2>Portfolio</h2>
         <p>A grid layout of project links</p>
       </section>
-      <section class='project-grid'>
+      <section className='project-grid'>
         <a
           href='https://kingofathousandworlds.wordpress.com/'
-          class='project-1 box-link'
-          target='_blank'></a>
-        <div class='project-1 preview'>
-          <div class='project-box'>
-            This project reflects and old attempt at creating a strategy gaming
-            blog, on top of an even older attempt to provide play-by-play
-            records of the games I played.
+          className='project-1 box-link'>
+          <div className='project-1 preview'>
+            <div className='project-box'>
+              This project reflects and old attempt at creating a strategy
+              gaming blog, on top of an even older attempt to provide
+              play-by-play records of the games I played.
+            </div>
           </div>
-        </div>
+        </a>
+
         <a
           href='https://www.rockpapershotgun.com/2016/04/26/civilization-v-community-patch-project/'
-          class='project-2 box-link'
-          target='_blank'></a>
-        <div class='project-2 preview'>
-          <div class='project-box'>
-            One of my proudest moments was writing a guest article for Rock,
-            Paper, Shotgun, one of the biggest (and one of the best) games
-            journalism sites out there.
+          className='project-2 box-link'>
+          <div className='project-2 preview'>
+            <div className='project-box'>
+              One of my proudest moments was writing a guest article for Rock,
+              Paper, Shotgun, one of the biggest (and one of the best) games
+              journalism sites out there.
+            </div>
           </div>
-        </div>
+        </a>
+
         <a
           href='https://unwinnable.com/2016/03/21/the-bad-luck-issue-theme-recap-77/'
-          class='project-3 box-link'
-          target='_blank'></a>
-        <div class='project-3 preview'>
-          <div class='project-box'>
-            Unwinnable is a digital subscription magazine that tries to focus on
-            the less-talked-about aspects of pop culture, and celebrates them.
-            My contribution to their 77th issue focuses on bad luck in games,
-            and why it might actually be all your fault.
+          className='project-3 box-link'>
+          <div className='project-3 preview'>
+            <div className='project-box'>
+              Unwinnable is a digital subscription magazine that tries to focus
+              on the less-talked-about aspects of pop culture, and celebrates
+              them. My contribution to their 77th issue focuses on bad luck in
+              games, and why it might actually be all your fault.
+            </div>
           </div>
-        </div>
-        <a href='contact.html' class='project-4 box-link' target='_blank'></a>
-        <div class='project-4 preview'>
-          <div class='project-box'>
-            For now this links back to the contact page.
+        </a>
+
+        <a href='https://github.com/sgwicks' className='project-4 box-link'>
+          <div className='project-4 preview'>
+            <div className='project-box'>For now this links to github.</div>
           </div>
-        </div>
+        </a>
       </section>
       <section>
         <p></p>
