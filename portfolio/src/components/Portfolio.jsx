241b0636b5006ad0e845834c7e9780d0500a8c13
sgwicks <samgwicks@gmail.com>
2020-04-20 16:47:53 +0100

    Added hover effects (touchMove on mobile) for portfolio image cards

--- portfolio/src/components/Portfolio.jsx
+++ portfolio/src/components/Portfolio.jsx
@@ -1,41 +1,89 @@
-import React from 'react';
-import { ProjectGrid, ProjectBox, ProjectImg } from '../styles/PortfolioStyles';
+import React, { useState } from 'react';
+import {
+  ProjectGrid,
+  ProjectBox,
+  ProjectImg,
+  ProjectText
+} from '../styles/PortfolioStyles';
 import Alex from '../images/dom_alex_300.jpg';
+import William from '../images/William300.jpeg';
+import BadLuck from '../images/Bad-Luck300.jpg';
 
 const Portfolio = () => {
+  const [project, setProject] = useState('');
+  // const [touched, setTouched] = useState(false);
+
+  const handleMouse = (project) => {
+    setProject(project);
+  };
+
+  // const handleTouch = (event, project) => {
+  //   if (!touched) {
+  //     event.preventDefault();
+  //     setTouched(true);
+  //     setProject(project);
+  //   }
+  // };
+
   return (
     <main>
       <h2>Portfolio</h2>
       <ProjectGrid>
-        <ProjectBox href='https://kingofathousandworlds.wordpress.com/'>
-          <ProjectImg src={Alex} alt='Alex' />
-          <br />
-          This project reflects and old attempt at creating a strategy gaming
-          blog, on top of an even older attempt to provide play-by-play records
-          of the games I played.
+        <ProjectBox
+          href='https://kingofathousandworlds.wordpress.com/'
+          onMouseEnter={() => handleMouse('Blog')}
+          onTouchMove={() => handleMouse('Blog')}
+          onMouseLeave={() => handleMouse('')}>
+          <ProjectImg src={Alex} alt='Alexendar, Civ 5' />
+          {project === 'Blog' && (
+            <ProjectText>
+              This project reflects an old attempt at creating a strategy gaming
+              blog, on top of an even older attempt to provide play-by-play
+              records of the games I played.
+            </ProjectText>
+          )}
         </ProjectBox>
 
-        <ProjectBox href='https://www.rockpapershotgun.com/2016/04/26/civilization-v-community-patch-project/'>
-          <ProjectImg src={Alex} alt='Alex' />
-          <br />
-          One of my proudest moments was writing a guest article for Rock,
-          Paper, Shotgun, one of the biggest (and one of the best) games
-          journalism sites out there.
+        <ProjectBox
+          href='https://www.rockpapershotgun.com/2016/04/26/civilization-v-community-patch-project/'
+          onMouseEnter={() => handleMouse('RPS')}
+          onTouchMove={() => handleMouse('RPS')}
+          onMouseLeave={() => handleMouse('')}>
+          <ProjectImg src={William} alt='William of Orange, Civ 5' />
+          {project === 'RPS' && (
+            <ProjectText>
+              One of my proudest moments was writing a guest article for Rock,
+              Paper, Shotgun, one of the biggest (and one of the best) games
+              journalism sites out there.
+            </ProjectText>
+          )}
         </ProjectBox>
 
-        <ProjectBox href='https://unwinnable.com/2016/03/21/the-bad-luck-issue-theme-recap-77/'>
-          <ProjectImg src={Alex} alt='Alex' />
-          <br />
-          Unwinnable is a digital subscription magazine that tries to focus on
-          the less-talked-about aspects of pop culture, and celebrates them. My
-          contribution to their 77th issue focuses on bad luck in games, and why
-          it might actually be all your fault.
+        <ProjectBox
+          href='https://unwinnable.com/2016/03/21/the-bad-luck-issue-theme-recap-77/'
+          onMouseEnter={() => handleMouse('Unwinnable')}
+          onTouchMove={() => handleMouse('Unwinnable')}
+          onMouseLeave={() => handleMouse('')}>
+          <ProjectImg src={BadLuck} alt='Assassination of Caesar' />
+          {project === 'Unwinnable' && (
+            <ProjectText>
+              Unwinnable is a digital subscription magazine that tries to focus
+              on the less-talked-about aspects of pop culture, and celebrates
+              them. My contribution to their 77th issue focuses on bad luck in
+              games, and why it might actually be all your fault.
+            </ProjectText>
+          )}
         </ProjectBox>
 
-        <ProjectBox href='https://github.com/sgwicks'>
+        <ProjectBox
+          href='https://github.com/sgwicks'
+          onMouseEnter={() => handleMouse('Github')}
+          onTouchMove={() => handleMouse('Github')}
+          onMouseLeave={() => handleMouse('')}>
           <ProjectImg src={Alex} alt='Alex' />
-          <br />
-          For now this links to github.
+          {project === 'Github' && (
+            <ProjectText>For now this links to github.</ProjectText>
+          )}
         </ProjectBox>
       </ProjectGrid>
       <section>
--- portfolio/src/styles/PortfolioStyles.js
+++ portfolio/src/styles/PortfolioStyles.js
@@ -1,23 +1,41 @@
 import styled from 'styled-components';
 
 export const ProjectGrid = styled.section`
-  @media only screen and (max-width: 1100px) {
-    grid-template-columns: auto;
-  }
-
-  display: grid;
-  grid-gap: 1.5em;
-  grid-auto-rows: minmax(300px, auto);
-  grid-template-columns: 1fr 1fr;
+  justify-content: space-around;
+  align-items: space-around;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const ProjectBox = styled.a`
-  border: solid #999999 20px;
-  background-color: #ffffff;
-  opacity: 1;
-  padding: 2.5%;
+  box-shadow: 5px 5px 10px 10px #999;
+
+  border-radius: 5%;
+  color: black;
+  text-outline: white;
+  text-decoration: none;
+  position: relative;
+  margin: 20px;
 `;
 
 export const ProjectImg = styled.img`
+  height: 300px;
+  width: 300px;
+  float: left;
+  border-radius: 5%;
+  margin: 5px;
+`;
+
+export const ProjectText = styled.p`
+  position: absolute;
+  background-color: rgb(255, 255, 255, 0.5);
+  max-height: 100%;
   max-width: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-radius: 5%;
+  margin: 0;
+  padding: 15%;
 `;
